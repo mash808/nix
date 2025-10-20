2ec923847292212fb0ff0d4a1fd497e5af6e9d0d
mash808 <asheredwards01@gmail.com>
2025-10-20 18:10:26 +1000

    cleanup

--- cachix.nix
+++ cachix.nix
@@ -3,11 +3,11 @@
 { pkgs, lib, ... }:
 
 let
-  folder = ./cachix;
-  toImport = name: value: folder + ("/" + name);
-  filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
-  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
+folder = ./cachix;
+toImport = name: value: folder + ("/" + name);
+filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
+imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
 in {
-  inherit imports;
-  nix.settings.substituters = ["https://cache.nixos.org/"];
+	inherit imports;
+	nix.settings.substituters = ["https://cache.nixos.org/"];
 }
--- configuration.nix
+++ configuration.nix
@@ -5,164 +5,101 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-		./cachix.nix
-    ];
+	imports =
+		[
+			./hardware-configuration.nix
+			./cachix.nix
+		];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  # disable hibernation
-  systemd.targets.hibernate.enable = false;
-  systemd.targets."hybrid-sleep".enable = false;
-  systemd.sleep.extraConfig = ''
-  	AllowHibernation=no
-	AllowHybridSleep=no
-  '';
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+	networking.hostName = "nixos"; # Define your hostname.
+	networking.networkmanager.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+	time.timeZone = "Australia/Brisbane";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+	i18n.defaultLocale = "en_GB.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_AU.UTF-8";
+		LC_IDENTIFICATION = "en_AU.UTF-8";
+		LC_MEASUREMENT = "en_AU.UTF-8";
+		LC_MONETARY = "en_AU.UTF-8";
+		LC_NAME = "en_AU.UTF-8";
+		LC_NUMERIC = "en_AU.UTF-8";
+		LC_PAPER = "en_AU.UTF-8";
+		LC_TELEPHONE = "en_AU.UTF-8";
+		LC_TIME = "en_AU.UTF-8";
+	};
 
-  # Set your time zone.
-  time.timeZone = "Australia/Brisbane";
+	services.xserver.enable = true;
+	services.displayManager.sddm.enable = true;
+	services.desktopManager.plasma6.enable = true;
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
+	services.xserver.xkb = {
+		layout = "au";
+		variant = "";
+	};
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_AU.UTF-8";
-    LC_IDENTIFICATION = "en_AU.UTF-8";
-    LC_MEASUREMENT = "en_AU.UTF-8";
-    LC_MONETARY = "en_AU.UTF-8";
-    LC_NAME = "en_AU.UTF-8";
-    LC_NUMERIC = "en_AU.UTF-8";
-    LC_PAPER = "en_AU.UTF-8";
-    LC_TELEPHONE = "en_AU.UTF-8";
-    LC_TIME = "en_AU.UTF-8";
-  };
+	services.printing.enable = true;
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+	services.pulseaudio.enable = false;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+	};
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+	users.users.ash = {
+		isNormalUser = true;
+		description = "ash";
+		extraGroups = [ "networkmanager" "wheel" ];
+		shell = pkgs.nushell;
+		packages = with pkgs; [
+			kdePackages.kate
+		];
+	};
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "au";
-    variant = "";
-  };
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+	programs.firefox.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+	programs.ssh.startAgent = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+	nixpkgs.config.allowUnfree = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+	environment.systemPackages = with pkgs; [
+			neovim
+			wget
+			openssl
+			cmake
+			pkg-config
+			curl
+			git
+			nushell
+			fastfetch
+			ghostty
+			easyeffects
+	];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ash = {
-    isNormalUser = true;
-    description = "ash";
-    extraGroups = [ "networkmanager" "wheel" ];
-    shell = pkgs.nushell;
-    packages = with pkgs; [
-      kdePackages.kate
-		vlc
-		gimp
-    #  thunderbird
-    ];
-  };
+	# nushell is not POSIX compliant, so launch it from bash instead of setting as login shell
+	environment.shells = [
+		pkgs.nushell
+	];
+	programs.bash.interactiveShellInit = ''
+		if ! [ "$TERM" = "dumb" ]; then
+			exec nu
+				fi
+				'';
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # start OpenSSH agent at login
-  programs.ssh.startAgent = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    openssl
-    cmake
-    pkg-config
-    curl
-    git
-    nushell
-	 fastfetch
-    ghostty
-    easyeffects
-  ];
-
-  # nushell is not POSIX compliant, so launch it from bash instead of setting as login shell
-  environment.shells = [
-    pkgs.nushell
-  ];
-  programs.bash.interactiveShellInit = ''
-    if ! [ "$TERM" = "dumb" ]; then
-      exec nu
-    fi
-  '';
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "25.05"; # Did you read the comment?
 }
