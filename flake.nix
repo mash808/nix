ab772ccadfe7363d1a793f3bc6a5b07a81ff419d
mash808 <asheredwards01@gmail.com>
2025-10-20 16:10:29 +1000

    init config

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+	description = "Asher's NixOS system configuration";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs }: {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [ ./configuration.nix ];
+		};
+	};
+}
