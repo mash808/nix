3bdf6283d03cac4749abde76265383e4cf70ab61
mash808 <asheredwards01@gmail.com>
2025-10-20 18:13:57 +1000

    properly disable all sleep/suspend states at a systemd level

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,18 @@
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
+	# disable all sleep/suspend states
+	systemd.sleep.extraConfig = ''
+		AllowSuspend=no
+		AllowHibernation=no
+		AllowSuspendThenHibernate=no
+		AllowHybridSleep=no
+	'';
+
+	systemd.targets.sleep.enable = false;
+	systemd.targets.suspend.enable = false;
+	systemd.targets.hibernate.enable = false;
+	systemd.targets.hybrid-sleep.enable = false;
 
 	networking.hostName = "nixos"; # Define your hostname.
 	networking.networkmanager.enable = true;
